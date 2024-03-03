ec8f9f0c3099e8775b2ddeed743af71b31f7925e
M8te <38794725+m8tec@users.noreply.github.com>
2024-03-03 20:45:54 +0100

    Loop and search for updated playlists
    + Deezync will now loop over playlists and search for changes
    + Configure sync interval per playlist in your config.yaml
    + It will only sync updated playlists to Plex to save time
    + Added an exception in case of Deezer login problems
    + Detect playlist changes by comparing checksums (after first run)
    + Added time to logging

--- main.py
+++ main.py
@@ -16,14 +16,23 @@ import time
 
 import logging
 
-
-logging.basicConfig(level=logging.INFO)
+# Configure the logging format
+logging.basicConfig(
+    format='[%(levelname)s] %(asctime)s: %(message)s',
+    datefmt='%H:%M:%S',
+    level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 def deezer_login():
     deezer = deemixDeezer()
     deezer.login_via_arl(downloadArl.strip())
+
+    if not deezer.current_user:
+        logging.error("Couldn't log in to Deezer, check arl")
+        quit()
+
+    logging.info(f"Logged in to Deezer as {deezer.current_user['name']}")
 
     return deezer
 
@@ -41,18 +50,23 @@ def load_download_arl():
 
 
 def load_deezync_config():
-    # Check if the source file exists
     config_path = '/config/config.yaml'
+
+    # copy template config if no config exists
     if not os.path.isfile(config_path):
-        # If the file doesn't exist, copy it
-        shutil.copy('config.yaml', config_path)
-        shutil.copy('deemix/config.json', '/config/deemix/config.json')
+        shutil.copy('config.yaml', config_path)  # Deezync config
+        shutil.copy('deemix/config.json', '/config/deemix/config.json')  # Deemix config
 
-        logging.info(f"Restored template deemix/config.yaml, please prepare configs!")
+        logging.info(f"Restored template deemix/config.yaml, please set up configs!")
+        logging.info("Quit for user to set up configs")
 
-    logging.info(f"{config_path} already exists. Loading config...")
+        quit()
+
+    logging.debug(f"Loading config from {config_path}")
     with open(config_path, 'r') as deezync_config:
         loaded_config = yaml.safe_load(deezync_config)
+
+    logging.info("Loaded config")
     return loaded_config
 
 
@@ -63,97 +77,41 @@ downloadArl = load_download_arl()
 
 # login to deezer download account
 dz: deemixDeezer = deezer_login()
+plex_server = None
+
+downloaded_tracks = []
+cached_deezer_playlists = {}
+playlist_last_sync_time = {}
 
 
-def download(links, bitrate):
-    # generate download objects for URLs
-    downloadObjects = []
-    for link in links:
-        try:
-            # attempt to generate download object for the current URL
-            downloadObject = generateDownloadObject(dz, link, bitrate)
-        except GenerationError as e:
-            # skip link if errors occurs
-            logging.error(f"{e.link}: {e.message}")
-            continue
-        # append single object to the downloadObjects list
-        downloadObjects.append(downloadObject)
+def connect_plex():
+    global plex_server
 
-    # download objects
-    for obj in downloadObjects:
-        Downloader(dz, obj, settings).start()
-
-
-def file_contains_string(folder_path, search_string):
-    matching_files = []
-
-    # Walk through all files and directories in the given folder and its subdirectories
-    for root, dirs, files in os.walk(folder_path):
-        # Check files in the current directory for the search string
-        matching_files.extend([os.path.join(root, file) for file in files if search_string in file])
-
-        # Check subdirectories for the search string in their names
-        matching_files.extend([os.path.join(root, directory) for directory in dirs if search_string in dir])
-
-    return matching_files
-
-
-def download_deezer_playlists():
-    download_count = 0
-    for playlist_config in deezer_playlist_configs:
-        playlist_id = playlist_config['id']
-
-        for track in deezer_playlist_missing_tracks.get(playlist_id, []):
-            # skip download if track has already been attempted before
-            if downloaded_tracks.__contains__(track['id']):
-                continue
-            downloaded_tracks.append(track['id'])
-
-            logging.info(f"Downloading {track['title']} by {track['artist']['name']}...")
-
-            # download track
-            download([track['link']], playlist_config['bitrate'])
-            download_count = download_count + 1
-
-            logging.info(f"Downloaded {track['title']} by {track['artist']['name']}")
-
-    logging.info(f"Downloaded {download_count} new tracks")
-
-
-def fetch_deezer_playlists():
-    logging.info("Fetching playlists...")
-    playlists = []
-    for playlist_config in deezer_playlist_configs:
-        # skip if set inactive by user
-        if playlist_config['active'] == 0:
-            continue
-
-        # fetch playlist
-        try:
-            playlist = dz.api.get_playlist(playlist_config['id'])
-            playlists.append(playlist)
-        except Exception as e:
-            logging.info(f"Failed to fetch playlist {playlist_config['id']}: {e}")
-
-    logging.info(f"Fetched {len(playlists)} playlists")
-    return playlists
-
-
-def deezer_plex_sync():
     if not config['plex_token'] or not config['plex_token'] or not config['plex_token']:
-        logging.warning("Missing Plex credentials, skipping sync")
+        logging.error("Missing Plex credentials, won't sync")
         return
 
-    logging.info("Logging in to Plex...")
-    account = MyPlexAccount(token=config['plex_token'])
-    logging.info(f"Connecting to server {config['plex_server']}...")
-    plex_server = account.resource(config['plex_server']).connect()
-    logging.info("Connected to Plex")
+    if not plex_server:
+        logging.debug("Logging in to Plex")
+        account = MyPlexAccount(token=config['plex_token'])
+        logging.info("Logged in to Plex")
+
+        logging.debug(f"Connecting to server {config['plex_server']}")
+        plex_server = account.resource(config['plex_server']).connect()
+
+        logging.info(f"Connected to Plex server {config['plex_server']}")
+
+
+def deezer_plex_sync(deezer_playlists):
+    global plex_server
 
     missing_by_playlist = {}
 
+    sync_playlist_counter = 1
     for deezer_playlist in deezer_playlists:
-        logging.info(f"Syncing Deezer playlist '{deezer_playlist['title']}' to Plex...")
+        logging.info(f"Syncing {sync_playlist_counter}/{len(deezer_playlists)} Deezer playlisty "
+                     f"'{deezer_playlist['title']}' to Plex...")
+        sync_playlist_counter += 1
 
         # check if Plex playlist already exists
         plex_playlist = None
@@ -202,17 +160,157 @@ def deezer_plex_sync():
         if deezer_playlist['description']:
             plex_playlist.editSummary(deezer_playlist['description'])
 
+        # logging
         logging.info(
-            f"Finished syncing '{deezer_playlist['title']}' playlist, added: {len(found_plex_tracks)} items. "
-            f"Missing tracks ({len(missing_by_playlist[deezer_playlist['id']])}):")
+            f"Synced '{deezer_playlist['title']}' playlist. Added: {len(found_plex_tracks)}, missing: "
+            f"{len(missing_by_playlist[deezer_playlist['id']])}")
         for track in missing_by_playlist[deezer_playlist['id']]:
-            logging.debug(f"Title: {track['title']}, artist: {track['artist']['name']}")
+            logging.debug(f"Missing title: {track['title']}, artist: {track['artist']['name']}")
+
+        # remove empty playlist from missing tracks if no tracks missing
+        if len(missing_by_playlist[deezer_playlist['id']]) < 1:
+            missing_by_playlist.pop(deezer_playlist['id'])
 
     logging.info("Synced Deezer playlists to Plex")
     return missing_by_playlist
 
 
-# Load Deezync configuration from the YAML file
+def download(links, bitrate):
+    # generate download objects for URLs
+    downloadObjects = []
+    for link in links:
+        try:
+            # attempt to generate download object for the current URL
+            downloadObject = generateDownloadObject(dz, link, bitrate)
+        except GenerationError as e:
+            # skip link if errors occurs
+            logging.error(f"{e.link}: {e.message}")
+            continue
+        # append single object to the downloadObjects list
+        downloadObjects.append(downloadObject)
+
+    # download objects
+    for obj in downloadObjects:
+        Downloader(dz, obj, settings).start()
+
+
+def download_deezer_playlists(deezer_playlist_missing_tracks):
+    download_count = 0
+    for playlist_config in deezer_playlist_configs:
+        playlist_id = playlist_config['id']
+
+        for track in deezer_playlist_missing_tracks.get(playlist_id, []):
+            # skip download if track has already been attempted before
+            if downloaded_tracks.__contains__(track['id']):
+                continue
+            downloaded_tracks.append(track['id'])
+
+            logging.info(f"Download {track['title']} by {track['artist']['name']}...")
+
+            # download track
+            download([track['link']], playlist_config['bitrate'])
+            download_count = download_count + 1
+
+            logging.info(f"Downloaded {track['title']} by {track['artist']['name']}")
+
+    logging.info(f"Downloaded {download_count} new tracks")
+
+
+def file_contains_string(folder_path, search_string):
+    matching_files = []
+
+    # Walk through all files and directories in the given folder and its subdirectories
+    for root, dirs, files in os.walk(folder_path):
+        # Check files in the current directory for the search string
+        matching_files.extend([os.path.join(root, file) for file in files if search_string in file])
+
+        # Check subdirectories for the search string in their names
+        matching_files.extend([os.path.join(root, directory) for directory in dirs if search_string in dir])
+
+    return matching_files
+
+
+def loop():
+    connect_plex()
+    if not plex_server:
+        logging.error("Can't sync because no Plex server provided")
+
+    cycleCount = 1
+    while True:
+        logging.info(f"Starting sync cycle {cycleCount}")
+
+        # check playlists for updates
+        changed_deezer_playlists = update_playlists()
+        logging.info(f"Detected {len(changed_deezer_playlists)} playlist changes on Deezer")
+
+        # skip further sync if no changes detected
+        if not changed_deezer_playlists:
+            intervalSeconds = 20
+            logging.info(f"No changes detected, sleeping for {intervalSeconds} seconds...")
+            time.sleep(intervalSeconds)
+            pass
+
+        # sync playlists to Plex and find missing tracks
+        deezer_playlist_missing_tracks = deezer_plex_sync(changed_deezer_playlists)
+
+        if deezer_playlist_missing_tracks:
+            # download missing tracks
+            logging.info(f"Downloading {len(deezer_playlist_missing_tracks)} missing tracks")
+            download_deezer_playlists(deezer_playlist_missing_tracks)
+
+            # give Plex time to index new files
+            seconds = 60
+            logging.info(f"Wait {seconds} seconds until syncing playlist with new downloads")
+            time.sleep(seconds)
+
+            # resync playlists with new files
+            deezer_plex_sync(changed_deezer_playlists)
+
+        logging.info(f"Finished sync cycle {cycleCount}")
+        cycleCount = cycleCount + 1
+
+
+def update_playlists():
+    global cached_deezer_playlists
+    global playlist_last_sync_time
+
+    logging.info("Update playlist info from Deezer...")
+
+    playlists = []
+    update_count = 0
+    for playlist_config in deezer_playlist_configs:
+        # skip if set inactive by user
+        if playlist_config['active'] == 0:
+            continue
+
+        # skip if sync interval not reached yet
+        if playlist_last_sync_time.__contains__(playlist_config['id']):
+            seconds_between = time.time() - playlist_last_sync_time[playlist_config['id']]
+            if seconds_between < playlist_config['sync_interval_seconds']:
+                continue
+
+        # fetch playlist
+        try:
+            playlist = dz.api.get_playlist(playlist_config['id'])
+
+            # detect changes to playlist using checksum
+            if (not cached_deezer_playlists.__contains__(playlist_config['id']) or
+                    cached_deezer_playlists[playlist_config['id']] != playlist['checksum']):
+
+                # save changes and add to changed playlist queue
+                playlists.append(playlist)
+                cached_deezer_playlists[playlist_config['id']] = playlist['checksum']
+
+            playlist_last_sync_time[playlist_config['id']] = time.time()
+            update_count += 1
+        except Exception as e:
+            logging.info(f"Failed to fetch playlist {playlist_config['id']}: {e}")
+
+    logging.info(f"Updated {update_count} playlists")
+    return playlists
+
+
+# load Deezync configuration from the YAML file
 config = load_deezync_config()
 deezer_playlist_configs = config['deezer_playlists']
 
@@ -221,29 +319,9 @@ music_path = "/music"
 if not os.path.exists(music_path):
     raise FileNotFoundError(f"The specified music path '{music_path}' does not exist.")
 
-downloaded_tracks = []
-
+# quit if no playlists to sync
 if len(deezer_playlist_configs) < 1:
-    logging.info("No sync playlist configured, quitting")
+    logging.info("No sync playlist configured, quitting...")
     quit()
 
-# fetch playlists to sync
-deezer_playlists = fetch_deezer_playlists()
-if len(deezer_playlists) < 1:
-    logging.info("No playlists fetched, quitting")
-    quit()
-
-# sync playlists to Plex and find missing tracks
-deezer_playlist_missing_tracks = deezer_plex_sync()
-
-# download missing tracks
-if deezer_playlist_missing_tracks:
-    download_deezer_playlists()
-
-# resync playlists after 10 minutes, give Plex time to index new files
-seconds = 600
-logging.info(f"Waiting {seconds} seconds until playlist sync...")
-time.sleep(seconds)
-deezer_plex_sync()
-
-logging.info("Finished Deezer to Plex playlist sync")
+loop()
